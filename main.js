fb5b18b6a0eb5297b849c8c72f0ea6c93d6c0fcc
Andrew Appleby <andrewappleby22@gmail.com>
2015-10-06 14:03:00 -0400

    Reduced x-axis to day ticks, fixed date unix issue

--- main.js
+++ main.js
@@ -20,58 +20,75 @@ angular.module('weatherApp', ['angular-c3'])
 		// Load in our weather data
 		$scope.refreshWeatherData = function (city) {
 			var promise = dataService.getWeatherData(city);
-			console.log("promise", promise);
+
 			// Handle data promise
 			promise.then(
+				// Success
 				function (payload) {
-					$log.info("Success:", payload.data);
 
+					// Grab our data set
 					var rawData = _.clone(payload.data);
-
 					$log.info("Raw data:", rawData);
 
 					// Map out to object with date & temperature
 					var list = _.clone(rawData.list)
 					.map(function (listItem) {
 						return {
-							date: listItem.dt,
-							temp: listItem.main.temp
+							date: listItem.dt * 1000, // Convert unix s -> ms
+							temp: listItem.main.temp,
 						}
 					});
-
 					$log.info("List:", list);
 
 					c3Factory.get('chart').then(function(chart) {
 						console.log("chart", chart);
-					  chart.load({
-					    json: list,
-					    keys: {
-					    	x: 'date',
-					    	value: ['temp']
-					    }
-					  });
+						chart.load({
+							json: list,
+							keys: {
+								x: 'date',
+								value: ['temp'],
+							}
+						});
 					});
+
 				},
+				// Errors
 				function (err) {
 					$log.error('Failure loading weather data', err);
 				}			
 			);
 		}
+		
+
+		// Generate our day x-ticks (assuming 5-day forecast)
+		var daysArray = []
+		var thisDay = null
+		var today = moment().startOf('day');
+
+		for (var i = 0; i < 7; i++) {
+			thisDay = today.clone().add(i, 'days').valueOf();
+			daysArray.push(thisDay);
+		};
+
+		// Initial chart config before loading in JSON
+		$scope.chartConfig = {
+			data: {
+				json: [], // Empty until openweather request completed
+				type: 'area-spline',
+			},
+			axis: {
+				x: {
+					type: 'timeseries',
+					tick: {
+						format: function (timestamp) {
+							return moment(timestamp).format('Do MMM');
+						},
+						values: daysArray,
+					}
+				}
+			}
+		};
 
 		$scope.refreshWeatherData('Toronto');
 
-		// Initial chart config before loading in JSON
-		$scope.config = {
-		  data: {
-		    json: [], // Empty until openweather request completed		    
-		  },
-		  axis: {
-		      x: {
-		          type: 'timeseries',
-		          tick: {
-		              format: '%m/%d',
-		          }
-		      }
-		  }
-		};
 	})
